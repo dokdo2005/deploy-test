ad0d2f673316daac8cf907669cbb8a3c3b21477f
dokdo2005 <dokdo2005@gmail.com>
2021-03-23 15:08:33 +0900

    refresh bashrc after install nvm

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -3,6 +3,6 @@
 sudo apt update
 sudo apt install wget
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+source ~/.bashrc
 nvm install 14.16.0
 nvm use 14.16.0
